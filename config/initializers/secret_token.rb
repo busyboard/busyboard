c3c547b81b9bdcebd67064ada7d49b20a3777bbc
kernel13 <stephane.sop@free.fr>
2011-08-06 18:21:49 +0200

    Initial Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Busyboard::Application.config.secret_token = '75f631cec32f738bd871f081ebe32aed91dc2706e9fd9614a97e7f85eb4ee24c66ef58644398413774b4069de6f7364fdee2cea4a2ec17bd6bbb505026982437'
